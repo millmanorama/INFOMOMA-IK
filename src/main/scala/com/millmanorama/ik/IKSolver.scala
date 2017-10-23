41220871ae8091fa43f48fdaa8c0fa713b048abd
millmanorama <millmanorama@gmail.com>
2017-10-23 12:56:53 +0200

    seperate fk from drawing

--- src/main/scala/com/millmanorama/ik/IKSolver.scala
+++ src/main/scala/com/millmanorama/ik/IKSolver.scala
@@ -4,13 +4,14 @@ import breeze.numerics._
 import breeze.numerics.constants._
 import javafx.animation.AnimationTimer
 import javafx.application.Application
-import javafx.event.EventHandler
+
 import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 
 import javafx.application.Application
 import javafx.event.ActionEvent
 
+import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
@@ -71,26 +72,38 @@ class IKSolver extends Application {
         gc.setFill(Color.color(1, 1, 1, .1))
         gc.fillRect(0, 0, W, H)
         gc.setFill(Color.color(.5, .5, .5, 1))
-        gc.fillRect(0, oY+20, W, H )
-        //        for (i <- 1 until 10) {
-        //          gc.setFill(Color.color(0, 0, 0, .1))
-        //          gc.fillRect(0, 0, 900, 900)
+        gc.fillRect(0, oY + 20, W, H)
 
-        q = q + .1 * pinv(jacobian(lengths, q)) * (target - fk(lengths, q, gc))
-//        q(0) = clamp(q(0), -Pi / 2, Pi / 20)
-//        q(1) = clamp(q(1), -Pi / 2, Pi / 20)
-                        q(0) = q(0) % (2*Pi)
-                        q(1) = q(1) % (2*Pi)
-        q(2) = q(1) * 2 / 3.0
-        //        }
+        val targetDistance = norm(target)
+        val length = lengths.reduce(_ + _)
+
+        var points = fk(lengths, q)
+        var error = (target - points.last)
+        var errorMag = norm(error)
+        var i = 0
+        while (errorMag > 1) {
+          q = q + .1 * pinv(jacobian(lengths, q)) * error
+          q(0) = q(0) % (2 * Pi)
+          q(1) = q(1) % (2 * Pi)
+          q(2) = q(1) * 2 / 3.0
+
+          points = fk(lengths, q)
+          error = (target - points.last)
+          errorMag = norm(error)
+
+          i = i + 1
+        }
+
+        draw(points, gc)
         gc.setFill(Color.color(0, 0, 0, 1))
 
         val screenTarget = worldToScreen(target(0), target(1))
         gc.fillOval(screenTarget._1 - 5, screenTarget._2 - 5, 10.0, 10.0)
 
         gc.fillText("?s: " + q(0) + ",  " + q(1), 20, 20)
+        gc.fillText("itration: " + i, 20, 40)
+        gc.fillText("errorMag:" + errorMag, 20, 60)
         println(q)
-        //        gc.fillText("TIP: " + v2(0) + ", " + v2(1), 20, 40)
       }
     }.start
   }
@@ -98,7 +111,9 @@ class IKSolver extends Application {
   def clamp(x: Double, min: Double, max: Double): Double = {
     return Math.min(Math.max(min, x), max)
   }
-  def fk(lengths: List[Double], thetas: DenseVector[Double], gc: GraphicsContext): DenseVector[Double] = {
+
+  def fk(lengths: List[Double], thetas: DenseVector[Double]): List[DenseVector[Double]] = {
+    var points = List(o)
     var v1 = o
     var M = DenseMatrix.eye[Double](3)
     var v2: DenseVector[Double] = DenseVector(3)
@@ -107,17 +122,18 @@ class IKSolver extends Application {
       val T = translation(l, 0)
       M = M * R * T
       v2 = M * o
-
-      val v1Screen = worldToScreen(v1(0), v1(1))
-      val v2Screen = worldToScreen(v2(0), v2(1))
-      gc.setStroke(Color.RED)
-      gc.strokeLine(v1Screen._1, v1Screen._2, v2Screen._1, v2Screen._2)
+      points = points :+ v2
       v1 = v2
     }
-    return v2
+    return points
+  }
 
-    //    gc.fillText("?m: " + thetas(0) + " ?i: " + thetas(1) + " ?d: " + thetas(2), 20, 20)
-    //    gc.fillText("TIP: " + v2(0) + ", " + v2(1), 20, 40)
+  def draw(points: List[DenseVector[Double]], gc: GraphicsContext) {
+    var v1 = worldToScreen(points(0))
+    for (v2 <- points.tail.map(worldToScreen(_))) {
+      gc.strokeLine(v1._1, v1._2, v2._1, v2._2)
+      v1 = v2
+    }
   }
 
   def translation(x: Double, y: Double): DenseMatrix[Double] = {
@@ -126,6 +142,10 @@ class IKSolver extends Application {
       0, 1, 0,
       x, y, 1
     ))
+  }
+
+  def worldToScreen(p: DenseVector[Double]): (Double, Double) = {
+    return worldToScreen(p(0), p(1))
   }
 
   def worldToScreen(x: Double, y: Double): (Double, Double) = {
